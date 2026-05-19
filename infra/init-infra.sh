7d9833a07afc5794cbfaae9dcc7dd52aa76f2549
Seom88 <cesarleonardo88@hotmail.com>
2026-05-18 22:08:55 -0500

    chore: comment out storage class application in init-infra.sh

--- infra/init-infra.sh
+++ infra/init-infra.sh
@@ -6,10 +6,10 @@ echo "Infra: Applying changes to the cluster..."
 echo "Installing storageClass..."
 if kubectl get nodes -o jsonpath='{.items[0].metadata.labels.minikube\.k8s\.io/name}' | grep -q "minikube"; then
     echo "Environment detected: Minikube"
-    kubectl apply -f infra/storage/storage-minikube.yaml
+    # kubectl apply -f infra/storage/storage-minikube.yaml
 elif kubectl get nodes -o jsonpath='{.items[0].metadata.labels.k3s\.io/hostname}' &>/dev/null; then
     echo "Environment detected: K3s"
-    kubectl apply -f infra/storage/storage-k3s.yaml
+    # kubectl apply -f infra/storage/storage-k3s.yaml
 else
     echo "Unknown environment. No StorageClass applied."
     exit 1
