8e4c37dafe7caa74cee313a51ca7d87c8842c8d7
Seom88 <cesarleonardo88@hotmail.com>
2026-05-19 23:55:08 -0500

    feat: add Talos environment support with local-path-provisioner and update Prometheus storage configuration

--- infra/init-infra.sh
+++ infra/init-infra.sh
@@ -2,9 +2,15 @@
 
 echo "Infra: Applying changes to the cluster..."
 
-if kubectl get nodes -o jsonpath='{.items[0].metadata.labels.minikube\.k8s\.io/name}' | grep -q "minikube"; then
-    echo "Environment detected: Minikube"
-elif kubectl get nodes -o jsonpath='{.items[0].metadata.labels.k3s\.io/hostname}' &>/dev/null; then
+if kubectl get nodes -o jsonpath='{.items[0].metadata.labels.kubernetes\.io/hostname}' | grep -q "talos"; then
+    echo "Environment detected: Talos"
+    echo "Installing Local Path Provisioner..."
+    kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.30/deploy/local-path-storage.yaml
+    echo "Setting privileged security for Local Path Provisioner..."
+    kubectl label --overwrite ns local-path-storage pod-security.kubernetes.io/enforce=privileged
+    echo "Patching Local Path Provisioner to be default..."
+    kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+elif kubectl get nodes -o jsonpath='{.items[0].status.nodeInfo.kubeletVersion}' | grep -q "+k3s"; then
     # Follow https://docs.k3s.io/upgrades/automated
     echo "Environment detected: K3s"
     echo "Applying system upgrade controller CRDs and controller..."
@@ -12,6 +18,8 @@ elif kubectl get nodes -o jsonpath='{.items[0].metadata.labels.k3s\.io/hostname}
         -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/system-upgrade-controller.yaml
     echo "Applying update plan..."
     kubectl apply -f infra/update/plan-update-k3s.yaml
+elif kubectl get nodes -o jsonpath='{.items[0].metadata.labels.minikube\.k8s\.io/name}' | grep -q "minikube"; then
+    echo "Environment detected: Minikube"
 else
     echo "Unknown environment."
 fi
